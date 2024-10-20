de4eb228f44a56b79ab8e842da120de08e3b093b
Soybrayangit <fernando232521@gmail.com>
2024-10-19 20:34:43 -0400

    Se agrega el Proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_portafolio.Models;
+using System.Diagnostics;
+
+namespace MVC_portafolio.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Bienvenido()
+		{
+			return View();
+		 }
+        public IActionResult Pilares()
+        {
+			return View();
+        }
+        public IActionResult Aprendido()
+        {
+			return View();
+        }
+        public IActionResult Datos()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+		{
+			return View();
+		}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
